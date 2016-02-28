f104592bb10ae95887be771405fa8aa2bee0a3cb
root <root@debian>
2016-02-28 10:27:56 +0000

    wut

--- postgenerator/masterdecider.sh
+++ postgenerator/masterdecider.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-
+. functions.cfg
 terms="$@"
 
-php arethere100listings.php $terms
+howmanyebayresults $terms
 if [ $? == 1 ]
 then
 echo Not 100 Listings.
 exit 1
 fi
-domain=`./termstodomain.sh $terms`
-./isdomainavailable.sh $domain
+domain=`termstodomain $terms`
+isdomainavailable $domain
 if [ $? == 1 ]
 then
 exit 1
