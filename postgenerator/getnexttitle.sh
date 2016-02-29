ca10e08c01a1471622de5c2d54aae1e3a1d6b681
root <root@debian>
2016-02-29 01:35:59 +0000

    wut

--- postgenerator/getnexttitle.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-. functions.cfg
-function getnexttitle {
-	nexttitle=`head -n 1 /tmp/titles.txt`
-	echo $nexttitle
-	sed '1d' /tmp/titles.txt > tmpfile; mv tmpfile /tmp/titles.txt
-	}
-
-getnexttitle
